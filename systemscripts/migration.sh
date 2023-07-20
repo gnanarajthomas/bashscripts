fa09e337aa59e7b434a415c28be835a96cbb798d
gnanaraj <raj2428@gmail.com>
2023-07-21 01:35:53 +0530

    Historical Resource Utilization Module Added [Day-2]

--- systemscripts/migration.sh
+++ systemscripts/migration.sh
@@ -138,6 +138,46 @@ echo ""
 IFS=$OFS
 }
 
+########
+#Day -2#
+########
+
+#################################
+#Historical Resource Utilization#
+#################################
+
+sar_fn()
+{
+                SAR_DAYS=$(ls $SAR_LOG | wc -l)
+                if [[ $SAR_DAYS -ge 1 ]]; then
+                        echo "HIGH CPU Utilization for last $SAR_DAYS days:"
+                        ls $SAR_LOG | while read line; do
+                                CPU_IDLE=$(sar -u -f $line | grep '^[0-9]' | sed -e 's/AM\|PM//g' -e '/RESTART\|idle/d' | sort -s -n -k8,8 | awk '{ if (NR==1) print $8 }')
+                                CPU_PERCENT=$(echo "100.00 - $CPU_IDLE" | bc)
+                                echo "$line: $CPU_PERCENT%";
+                        done
+                else
+                        echo "No SAR files found"
+                fi
+
+}
+
+sar_report()
+{
+        if [[ $OS_DISTRO =~ Cent|Red|Alma|Rocky ]]; then
+                SAR_LOG="/var/log/sa/sa[0-3][0-9]"
+                sar_fn
+        elif [[ $OS_DISTRO =~ Ubuntu|Debian ]]; then
+                SAR_LOG="/var/log/sysstat/sa[0-3][0-9]"
+                sar_fn
+        else
+                echo "No SAR Found"
+        fi
+}
+
+
+
 os_type
 hw_details
 server_resources
+sar_report
