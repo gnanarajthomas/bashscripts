164217a9c3bf5add349b1aab2aeae45311806ebb
gnanaraj <raj2428@gmail.com>
2023-07-22 20:41:35 +0530

    Hash added

--- systemscripts/migration.sh
+++ systemscripts/migration.sh
@@ -104,6 +104,7 @@ e_e
 
 application_version()
 {
+print_hash_line
 test -f /usr/sbin/httpd         && httpd -v 2> /dev/null | awk '{if (NR==1) printf ("Installed Apache Version: \n%s\n",$3)}'
 test -f /usr/sbin/apache2ctl    && apache2ctl -v 2> /dev/null | awk '{if (NR==1) printf ("Installed Apache Version: \n%s\n",$3)}'
 test -f /usr/bin/mysql          && echo "Installed Database Version:";mysql -V | awk -F "for|," '{print $1}'
@@ -111,14 +112,17 @@ test -f /usr/sbin/varnishd      && echo "Installed Varnish Version:";varnishd -V
 test -f /usr/sbin/nginx         && nginx -v 2>&1 | awk '{printf ("Installed Nginx Version: \n%s\n",$3)}'
 test -f /usr/bin/redis-server   && redis-server --version | awk '{printf ("Installed Redis Version \n%s\n", $3)}'
 test -f /usr/sbin/php-fpm       && /usr/sbin/php-fpm -v | awk '/^PHP/ {printf ("Installed PHP-FPM Version \n%s\n", $2)}'
+e_e
 }
 
 php_apache_modules()
 {
+print_hash_line
 test -f /usr/sbin/php-fpm 	&& echo "Installed PHP-FPM Modules:" 	&& /usr/sbin/php-fpm -m
 test -f /usr/sbin/php 		&& echo "Installed PHP Modules:" 	&& /usr/sbin/php -m
 test -f /usr/sbin/httpd 	&& echo "Loaded Apache Modules:" 	&& /usr/sbin/httpd -M
 test -f /usr/sbin/apache2ctl 	&& echo "Loaded Apache Modules:" 	&& /usr/sbin/apache2ctl -M
+e_e
 }
 
 
